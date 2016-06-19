e706f6dbeb187136be0dc30f2280b73474602eeb
Michael Knoch <info@michaelknoch.de>
2016-06-19 19:02:16 +0200

    implement applicationsview page

--- app/pages/applicationsView/applicationsView.ts
+++ app/pages/applicationsView/applicationsView.ts
@@ -1,11 +1,11 @@
 import {Component} from "@angular/core";
 import {NavController, NavParams} from 'ionic-angular';
 import {SystemPicker} from 'mia-distributed/comp/systemList/systemPicker/systemPicker.comp'
-import {Graph} from "mia-distributed/comp/graph/graph.comp";
+import {ApplicationList} from "mia-distributed/comp/applicationList/applicationList.comp";
 
 @Component({
-    templateUrl: 'build/pages/graphView/graphView.html',
-    directives: [Graph]
+    templateUrl: 'build/pages/applicationList/applicationList.html',
+    directives: [ApplicationList]
 })
 export class GraphView {
 
