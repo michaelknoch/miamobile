c339b4c6d759ffbe07f6f1a237428df201e85da4
Michael Knoch <info@michaelknoch.de>
2016-08-11 13:41:06 +0200

    improve touchid comp

--- app/components/touchId/touchId.comp.ts
+++ app/components/touchId/touchId.comp.ts
@@ -1,40 +1,35 @@
 import {Component, OnInit} from '@angular/core'
 import {Platform} from 'ionic-angular';
 import {NgZone} from '@angular/core';
-
+import {UserService} from "mia-distributed/sharedServices/user.service";
 declare var touchid: any;
 
 @Component({
     selector: 'touch-id',
-    template: '<div [ngClass]="{active: getLock()}" class="lock">{{lock}}, {{getLock()}}</div>'
+    template: '<div [ngClass]="{active: getLock()}" class="lock"></div>'
 })
 
 export class TouchId implements OnInit {
 
     private lock = false;
 
-    constructor(private ngZone: NgZone, private platform: Platform) {
+    constructor(private ngZone: NgZone, private platform: Platform, private _userService: UserService) {
     }
 
     ngOnInit() {
         this.platform.ready().then(() => {
-            console.info('ready');
             touchid.checkSupport(() => {
-                console.info('check support');
                 this.platform.resume.subscribe(data => {
-
-                    console.info('resume');
-                    this.lock = true;
-
-                    touchid.authenticate((result) => {
-                        console.info('unlock success');
-                        this.ngZone.run(() => {
-                            this.lock = false;
+                    if (this._userService.getLocalMe().user.id) {
+                        this.lock = true;
+                        touchid.authenticate((result) => {
+                            console.info('unlock success');
+                            this.ngZone.run(() => {
+                                this.lock = false;
+                            });
                         });
-                    });
-
+                    }
                 });
-
             });
         });
     }
